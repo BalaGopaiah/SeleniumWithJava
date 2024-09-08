e886d711e89068808ef0a7b0d04f408da4fbd801
Balu <bala.g@zelarsoft.com>
2024-09-08 20:42:41 +0530

    Modified FDCalculator file and added config and log4j2 files in resources folder

--- FDCalculator.java
+++ FDCalculator.java
@@ -1,8 +1,11 @@
 package Calculator;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,13 +18,20 @@ public class FDCalculator {
 	
 		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("excludeSwitches", new String[] {"enable-automation"});
+		options.addArguments("--disable-notifications");
 		
-		WebDriver driver = new ChromeDriver();
+		FileReader file = new FileReader(".//src//test//resources//config.properties");
+		Properties prop = new Properties();
+		prop.load(file);
+		
+		
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://www.moneycontrol.com/fixed-income/calculator/state-bank-of-india-sbi/fixed-deposit-calculator-SBI-BSB001.html?classic=true");
+		driver.get(prop.getProperty("calcuURL"));
+		//driver.get("https://www.moneycontrol.com/fixed-income/calculator/state-bank-of-india-sbi/fixed-deposit-calculator-SBI-BSB001.html?classic=true");
 		driver.manage().window().maximize();
-		
-		String filePath = System.getProperty("user.dir")+"\\testdata\\CalData.xlsx";
+			
+		String filePath = System.getProperty("user.dir")+"\\testdata\\CalculateData.xlsx";
 		
 		int rows = ExcelUtils.getRowCount(filePath, "Sheet1");
 		
@@ -36,38 +46,38 @@ public class FDCalculator {
 			String exp_mvalue = ExcelUtils.getCellData(filePath, "Sheet1", i, 5);
 			
 			// pass above data into application
-			driver.findElement(By.xpath("//input[@id='principal']")).sendKeys(pric);
-			driver.findElement(By.xpath("//input[@id='interest']")).sendKeys(rateofinterest);
-			driver.findElement(By.xpath("//input[@id='tenure']")).sendKeys(per1);
+			driver.findElement(By.xpath(prop.getProperty("principle"))).sendKeys(pric);
+			driver.findElement(By.xpath(prop.getProperty("rateofinterest"))).sendKeys(rateofinterest);
+			driver.findElement(By.xpath(prop.getProperty("per1"))).sendKeys(per1);
 			
-			Select predrp = new Select(driver.findElement(By.xpath("//select[@id='tenurePeriod']")));
+			Select predrp = new Select(driver.findElement(By.xpath(prop.getProperty("per2"))));
 			predrp.selectByVisibleText(per2);
 			
-			Select fredrp = new Select(driver.findElement(By.xpath("//select[@id='frequency']")));
+			Select fredrp = new Select(driver.findElement(By.xpath(prop.getProperty("frequency"))));
 			fredrp.selectByVisibleText(fre);
 			
-			// driver.findElement(By.cssSelector("img[src='https://images.moneycontrol.com/images/mf_revamp/btn_calcutate.gif']")).click();
-			driver.findElement(By.xpath("//div[@class='CTR PT15']//a[1]")).click();  // clicked on calculate
-			// driver.findElement(By.xpath("//img[@class='PL5']")).click();
-			
+			driver.findElement(By.xpath(prop.getProperty("calculatebtn"))).click();  // clicked on calculate
+						
 			// Validation
-			String act_mvalue = driver.findElement(By.xpath("//span[@id='resp_matval']//strong")).getText();
+			String act_mvalue = driver.findElement(By.xpath(prop.getProperty("actvalue"))).getText();
 			
 			if( Double.parseDouble(exp_mvalue) == Double.parseDouble(act_mvalue))
 			{
 				System.out.println("Test passed");
 				ExcelUtils.setCellData(filePath, "Sheet1", i, 7, "Passed");
+				Thread.sleep(2000);
 				ExcelUtils.fillGreenColor(filePath, "Sheet1", i, 7);
 			}
 			else
 			{
 				System.out.println("Test Failed");
 				ExcelUtils.setCellData(filePath, "Sheet1", i, 7, "Failed");
+				Thread.sleep(2000);
 				ExcelUtils.fillRedColor(filePath, "Sheet1", i, 7);
 			}
 			
 			Thread.sleep(3000);
-			driver.findElement(By.xpath("//img[@class='PL5']")).click();
+			driver.findElement(By.xpath(prop.getProperty("clear"))).click();
 		}
 		//ending of for loop
 		driver.quit();
